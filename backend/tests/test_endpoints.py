f68357e33bffbc52ebd5042694d6d712d056ea46
Raxylon <maikmrowetz@gmail.com>
2025-12-11 14:38:10 +0100

    Automatischer Test (pytest) zum backend hinzugefügt um die verbindung root und /hello zu testen

---
+++ backend/tests/test_endpoints.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_root_route():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hallo vonnnn deinem Backend"}
+
+def test_hello_route():
+    response = client.get("/hello")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hallo aus dem Backend!"}
